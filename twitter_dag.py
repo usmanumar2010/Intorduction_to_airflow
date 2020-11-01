03d88719498d16d9a41dac88cd556e3564bfebaf
usman2019 <uumar@macrosoftinc.com>
2020-11-02 01:47:58 +0500

    python operator for fetching data added

--- twitter_dag.py
+++ twitter_dag.py
@@ -4,6 +4,7 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.sensors.file_sensor import FileSensor
 from datetime import datetime
+import fetching_tweet
 
 # default_args are the default arguments applied to the Dag's tasks
 DAG_DEFAULT_ARGS = {
@@ -12,5 +13,8 @@ DAG_DEFAULT_ARGS = {
 }
 
 with DAG(dag_id="twitter_dag",schedule_interval="@daily",default_args=DAG_DEFAULT_ARGS) as dag:
-	waiting_for_tweets=FileSensor(task_id="waiting_for_tweets",fs_conn_id="fs_tweet"	,filepath="data.csv",poke_interval=5)
+	waiting_for_tweets=FileSensor(task_id="waiting_for_tweets",fs_conn_id="fs_tweet",filepath="data.csv",poke_interval=5)
 
+	fetching_tweet_task = PythonOperator(task_id="fetching_tweet_task", python_callable=fetching_tweet.main)
+
+	cleaning_tweets = PythonOperator(task_id="cleaning_tweets", python_callable=cleaning_tweet.main)
